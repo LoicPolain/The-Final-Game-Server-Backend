d92ddc309c6f586790b4a0b7c229edf3e2a5ab1f
LoicPolain <loic.polain@student.ehb.be>
2024-01-29 20:33:08 +0100

    Feat: tests with postman

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 import { WebSocket, WebSocketServer } from "ws";
 const ws = new WebSocketServer({port: 8080});
 
+let dedicatedServer = {
+    port: 7777,
+    status: "Running",
+    playerStatus: "Awaiting",
+    playersCount: 0
+}
+
 ws.on("connection", (ws)=> {
-    console.log("user connected");
-    ws.send("Your are connected!")
+    console.log("New user connected");
+    ws.send("Your are connected to your own websocket!")
+    dedicatedServer.playersCount++
+    if (dedicatedServer.playersCount == 2) {
+        dedicatedServer.playerStatus = "Full"
+    }
+    ws.send(JSON.stringify(dedicatedServer))
 })

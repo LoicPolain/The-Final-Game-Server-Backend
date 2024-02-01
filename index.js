e29d3a5de3e157b91818014970d0ea5b5a2c256f
LoicPolain <loic.polain@student.ehb.be>
2024-02-01 08:42:04 +0100

    Feat: ws close event

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 import { WebSocket, WebSocketServer } from "ws";
-const ws = new WebSocketServer({port: 8080});
+const ws = new WebSocketServer({ port: 8080 });
 
 let dedicatedServer = {
-    port: 7777,
-    status: "Running",
-    playerStatus: "Awaiting",
-    playersCount: 0
-}
+  port: 7777,
+  status: "Running",
+  playersStatus: "Awaiting players",
+  playersCount: 0,
+};
 
-ws.on("connection", (ws)=> {
-    console.log("New user connected");
-    ws.send("Your are connected to your own websocket!")
-    dedicatedServer.playersCount++
+ws.on("connection", (ws) => {
+  console.log("New user connected");
+  ws.send("Your are connected to your own websocket!");
+
+  if (dedicatedServer.playersCount >= 2) {
+    ws.send("Server is full");
+  } else {
+    dedicatedServer.playersCount++;
     if (dedicatedServer.playersCount == 2) {
-        dedicatedServer.playerStatus = "Full"
+      dedicatedServer.playersStatus = "Full";
     }
-    ws.send(JSON.stringify(dedicatedServer))
-})
+  }
+
+  ws.on('close', function close() {
+    console.log('User disconnected');
+    dedicatedServer.playersCount--;
+    if (dedicatedServer.playersCount < 2) {
+      dedicatedServer.playersStatus = "Awaiting players";
+    }
+  });
+  ws.send(JSON.stringify(dedicatedServer));
+});
